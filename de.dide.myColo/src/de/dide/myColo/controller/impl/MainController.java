ea95b54f444c3b2eb8bebaca864e9b7879af87b6
Private Stash <virtualPrivateStash@gmail.com>
2016-03-11 19:44:41 +0100

    refactoring on move-functions in maincontroller

--- de.dide.myColo/src/de/dide/myColo/controller/impl/MainController.java
+++ de.dide.myColo/src/de/dide/myColo/controller/impl/MainController.java
@@ -24,77 +24,194 @@ public class MainController extends Observable implements Controller{
 		//return(cell.getNameOfTerrainType());
 		return "dummyString";
 	} 
-
+	
+	//////////////////////////////
+	//FUNCTIONS FOR MOVING UNITS//
+	//////////////////////////////
 	public void moveUnit_Left(Unit unit) {
-		moveUnit(unit, LEFT, false);
-	}
+		moveUnit(unit, LEFT, null);
+	}	
 
 	public void moveUnit_Right(Unit unit) {
-		moveUnit(unit, RIGHT, false);
+		moveUnit(unit, RIGHT, null);
 	}
 	
 	public void moveUnit_Up(Unit unit) {
-		moveUnit(unit, UP, false);
+		moveUnit(unit, UP, null);
 	}
 	
 	public void moveUnit_Down(Unit unit) {
-		moveUnit(unit, DOWN, false);
+		moveUnit(unit, DOWN, null);
 	}
 	
 	public void moveUnit_Up_Left(Unit unit) {
-		moveUnit(unit, UP, true);
-		moveUnit(unit, LEFT, true);
-		unit.decrMovesRemaining();
-	}
-
+		moveUnit(unit, UP, LEFT);	}
+	
 	public void moveUnit_Up_Right(Unit unit) {
-		moveUnit(unit, UP, true);
-		moveUnit(unit, RIGHT, true);
-		unit.decrMovesRemaining();
+		moveUnit(unit, UP, RIGHT);
 	}
 	
 	public void moveUnit_Down_Left(Unit unit) {
-		moveUnit(unit, DOWN, true);
-		moveUnit(unit, LEFT, true);
-		unit.decrMovesRemaining();
-	}
-
-	public void moveUnit_Down_Right(Unit unit) {
-		moveUnit(unit, DOWN, true);
-		moveUnit(unit, RIGHT, true);
-		unit.decrMovesRemaining();
+		moveUnit(unit, DOWN, LEFT);
 	}
 	
-	private void moveUnit(Unit unit, String direction, boolean dontDecr) {
-
+	public void moveUnit_Down_Right(Unit unit) {
+		moveUnit(unit, DOWN, RIGHT);
+	}
+	
+		
+	private void moveUnit(Unit unit, String direction1, String direction2) {
+		boolean dontDecr = true;
+		boolean positionChanged = false;
+		boolean twoDirections =  false;
+		boolean makeMove = false;
+		
+		//check if one or two directions were given
+		if (direction2 != null) {
+			twoDirections = true;
+		}
+		
+		if (!isMovePossible(unit, direction1)) {
+			makeMove = false;
+		} 
+		else {
+			if (!twoDirections) {
+				makeMove = true;
+			} else {
+				if (isMovePossible(unit, direction2)) {
+					makeMove = true;
+				}
+			}
+		}
+		
+	
+		//do the actual moves in this if clause
+		if (makeMove) {
+	
+			//STEP 1
+			moveUnitOneStep(unit, direction1);
+	
+			//STEP 2 IF GIVEN
+			if (twoDirections) {
+				moveUnitOneStep(unit, direction2);
+			}
+	
+			//UPDATE MODELL AND NOTIFY OBSERVERS
+			unit.decrMovesRemaining();
+			this.notifyObservers();					
+		}
+	}
+			
+			
+	private boolean isMovePossible(Unit unit, String direction) {
+		boolean possible = false;
 		switch (direction) {
 			case LEFT:
 				if (unit.getCoordY() > 0) {
-				unit.setCoordY(unit.getCoordY() - 1);	
+				possible = true; 	
 				}			
 				break;
 			case RIGHT:
 				if (unit.getCoordY() < Tui.getGameFieldSize() - 1) {
-				unit.setCoordY(unit.getCoordY() + 1);	
+				possible = true;
 				}			
 				break;
 			case DOWN:
 				if (unit.getCoordX() < Tui.getGameFieldSize() - 1) {
-				unit.setCoordX(unit.getCoordX() + 1);	
+				possible = true;
 				}			
 				break;
 			case UP:
 				if (unit.getCoordX() > 0) {
-				unit.setCoordX(unit.getCoordX() - 1);	
+				possible = true;
 				}			
+		}
+		return possible;
+	}
+	
+	
+	private void moveUnitOneStep(Unit unit, String direction) {
+		switch (direction) {
+			case LEFT:
+				unit.setCoordY(unit.getCoordY() - 1);	
+				break;
+			case RIGHT:
+				unit.setCoordY(unit.getCoordY() + 1);	
+				break;
+			case DOWN:
+				unit.setCoordX(unit.getCoordX() + 1);
+				break;
+			case UP:
+				unit.setCoordX(unit.getCoordX() - 1);
 				break;
 		}
-		if ( dontDecr == false) {
-			unit.decrMovesRemaining();
-		}
-		this.notifyObservers();
 	}
-
+	
+			
+	
+//	public void moveUnit_Up_Left(Unit unit) {
+//		moveUnit(unit, UP, true);
+//		moveUnit(unit, LEFT, true);
+//		unit.decrMovesRemaining();
+//	}
+//
+//	public void moveUnit_Up_Right(Unit unit) {
+//		moveUnit(unit, UP, true);
+//		moveUnit(unit, RIGHT, true);
+//		unit.decrMovesRemaining();
+//	}
+//	
+//	public void moveUnit_Down_Left(Unit unit) {
+//		moveUnit(unit, DOWN, true);
+//		moveUnit(unit, LEFT, true);
+//		unit.decrMovesRemaining();
+//	}
+//
+//	public void moveUnit_Down_Right(Unit unit) {
+//		moveUnit(unit, DOWN, true);
+//		moveUnit(unit, RIGHT, true);
+//		unit.decrMovesRemaining();
+//	}
+//	
+//	private boolean moveUnit(Unit unit, String direction, boolean dontDecr) {
+//		boolean positionChanged = false;
+//		switch (direction) {
+//			case LEFT:
+//				if (unit.getCoordY() > 0) {
+//				unit.setCoordY(unit.getCoordY() - 1);	
+//				positionChanged = true;
+//				}			
+//				break;
+//			case RIGHT:
+//				if (unit.getCoordY() < Tui.getGameFieldSize() - 1) {
+//				unit.setCoordY(unit.getCoordY() + 1);	
+//				positionChanged = true;
+//				}			
+//				break;
+//			case DOWN:
+//				if (unit.getCoordX() < Tui.getGameFieldSize() - 1) {
+//				unit.setCoordX(unit.getCoordX() + 1);
+//				positionChanged = true;
+//				}			
+//				break;
+//			case UP:
+//				if (unit.getCoordX() > 0) {
+//				unit.setCoordX(unit.getCoordX() - 1);
+//				positionChanged = true;
+//				}			
+//				break;
+//		}
+//		if ( dontDecr == false) {
+//			unit.decrMovesRemaining();
+//		}
+//		this.notifyObservers();
+//		return positionChanged;
+//	}
+//
+//	
+	
+	
+	
 
 	@Override
 	public GameCell getGameCell(int row, int col) {
